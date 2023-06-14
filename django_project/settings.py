a3ef93f8fc55730214dbb6a3e2ae9b69f090d624
user <user@Ubuntu22.myguest.virtualbox.org>
2023-06-14 16:35:18 +0500

    add aws hsot ip

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [".pythonanywhere.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["16.170.161.252",".pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
